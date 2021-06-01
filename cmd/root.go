02a256ce35d17942acdf94896994e530f2bbaf34
Ben Drucker <bvdrucker@gmail.com>
2021-06-01 13:19:20 -0700

    create socat pod

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,17 @@
 package cmd
 
 import (
+	"flag"
+	"fmt"
+	"path/filepath"
+	"strconv"
+
 	"github.com/spf13/cobra"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 var cfgFile string
@@ -18,7 +28,62 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	Run: func(cmd *cobra.Command, args []string) {},
+	Args: cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		var kubeconfig *string
+		if home := homedir.HomeDir(); home != "" {
+			kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+		} else {
+			kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+		}
+		flag.Parse()
+
+		// use the current context in kubeconfig
+		config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		clientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		port, err := strconv.Atoi(args[1])
+		if err != nil {
+			panic(err.Error())
+		}
+
+		pod, err := clientset.CoreV1().Pods("default").Create(cmd.Context(), &v1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "port-forward-remote-",
+			},
+			Spec: v1.PodSpec{
+				Containers: []v1.Container{
+					{
+						Name:  "socat",
+						Image: "alpine/socat",
+						Args: []string{
+							fmt.Sprintf("tcp-listen:%d,fork,reuseaddr", port),
+							fmt.Sprintf("tcp-connect:%s:%d", args[0], port),
+						},
+						Ports: []v1.ContainerPort{
+							{
+								Name:          "forwarded",
+								ContainerPort: int32(port),
+							},
+						},
+					},
+				},
+			},
+		}, metav1.CreateOptions{})
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		cmd.Println(pod.Name)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/bendrucker/kubernetes-port-forward-remote
 
 go 1.16
 
-require github.com/spf13/cobra v1.1.3
+require (
+	github.com/spf13/cobra v1.1.3
+	k8s.io/api v0.21.1 // indirect
+	k8s.io/apimachinery v0.21.1
+	k8s.io/client-go v0.21.1
+)
