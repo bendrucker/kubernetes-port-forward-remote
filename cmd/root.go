7e84edda919ce99750d102ecd6c27556d6a6da52
Ben Drucker <bvdrucker@gmail.com>
2021-06-01 14:15:23 -0700

    implement pod waiting and port forward

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,22 @@
 package cmd
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"net/http"
 	"path/filepath"
 	"strconv"
+	"time"
 
 	"github.com/spf13/cobra"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/portforward"
+	"k8s.io/client-go/transport/spdy"
 	"k8s.io/client-go/util/homedir"
 )
 
@@ -82,7 +88,44 @@ to quickly create a Cobra application.`,
 			panic(err.Error())
 		}
 
-		cmd.Println(pod.Name)
+		defer func() {
+			// _ = clientset.CoreV1().Pods(pod.Namespace).Delete(cmd.Context(), pod.Name, metav1.DeleteOptions{})
+		}()
+
+		err = wait.PollImmediate(time.Second, time.Minute, func() (bool, error) {
+			p, err := clientset.CoreV1().Pods(pod.Namespace).Get(cmd.Context(), pod.Name, metav1.GetOptions{})
+			if err != nil {
+				return false, err
+			}
+
+			switch p.Status.Phase {
+			case v1.PodRunning:
+				fmt.Println("pod running")
+				return true, nil
+			case v1.PodFailed, v1.PodSucceeded:
+				return false, errors.New("pod completed")
+			}
+			return false, nil
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		transport, upgrader, err := spdy.RoundTripperFor(config)
+		if err != nil {
+			panic(err.Error())
+		}
+		dialer := spdy.NewDialer(upgrader, &http.Client{Transport: transport}, "POST", clientset.RESTClient().Post().Prefix("api/v1").Resource("pods").Namespace("default").Name(pod.Name).SubResource("portforward").URL())
+
+		fw, err := portforward.New(dialer, []string{fmt.Sprintf("0:%d", port)}, make(chan struct{}), make(chan struct{}), cmd.OutOrStdout(), cmd.ErrOrStderr())
+		if err != nil {
+			panic(err.Error())
+		}
+
+		err = fw.ForwardPorts()
+		if err != nil {
+			panic(err.Error())
+		}
 	},
 }
 
